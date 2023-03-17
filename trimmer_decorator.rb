605e95c847fe45fd8bf7be0108139a9ab98b8348
Dell Pc <mudasir9757@gmail.com>
2023-03-17 23:00:30 +0500

    fix linter 2

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -2,7 +2,6 @@ require_relative 'base_decorator'
 
 class TrimmerDecorator < BaseDecorator
   MAX_NAME_LENGTH = 10
-
   def correct_name
     namee = @nameable.correct_name
     if namee.length > MAX_NAME_LENGTH
