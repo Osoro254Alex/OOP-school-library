02ebc6fc4060954daed3813463206cfba4b7d227
Dell Pc <mudasir9757@gmail.com>
2023-03-17 23:06:11 +0500

    changes 3

--- option.rb
+++ option.rb
@@ -6,7 +6,7 @@ class Option
     puts '3. Create a person'
     puts '4. Create a book'
     puts '5. Create a rental'
-    puts '6. List all rentals for a given person id'
+    puts '6. List all rentals for a given Person id'
     puts '7. Exit'
   end
 end
