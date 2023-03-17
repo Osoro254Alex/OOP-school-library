fd12032aa8cbafd3ad1a37ebff9f48fbbbbe8ad0
Dell Pc <mudasir9757@gmail.com>
2023-03-17 22:58:39 +0500

    linter fix

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -4,11 +4,11 @@ class TrimmerDecorator < BaseDecorator
   MAX_NAME_LENGTH = 10
 
   def correct_name
-    name = @nameable.correct_name
-    if name.length > MAX_NAME_LENGTH
-      name.slice(0, MAX_NAME_LENGTH)
+    namee = @nameable.correct_name
+    if namee.length > MAX_NAME_LENGTH
+      namee.slice(0, MAX_NAME_LENGTH)
     else
-      name
+      namee
     end
   end
 end
